33a065e2bfa69c6d0124b64c4836dc49d8ea42e5
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-23 12:04:07 +0600

    send data to server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  const handleAddUser = e => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { name: name, email: email }
+
+    // send data to the server
+    fetch('http://localhost:5000/users', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const addedUser = data;
+        const newUsers = [...users, addedUser];
+        setUsers(newUsers);
+        // reset name and email
+        nameRef.current.value = '';
+        emailRef.current.value = '';
+      })
+
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
+      <h2>Found Users: {users.length}</h2>
 
+      <form onSubmit={handleAddUser}>
+        <input type="text" ref={nameRef} placeholder="name" required />
+        <input type="email" ref={emailRef} name="" id="" placeholder="Email" required />
+        <input type="submit" value="Submit" />
+      </form>
+
+      <ul>
+        {
+          users.map(user => <li key={user.id}>{user.id}: {user.name} {user.email}</li>)
+        }
+      </ul>
     </div>
   );
 }
